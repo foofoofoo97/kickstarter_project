0b461186ed2093e76517b0382b8f9aaf3a8fdbf5
foofoofoo <fooziqin@gmail.com>
2021-09-24 10:45:56 +0800

    commit

--- kickstart/ethereum/web3.js
+++ kickstart/ethereum/web3.js
@@ -1,7 +1,17 @@
 import Web3 from "web3";
 
-window.ethereum.request({ method: "eth_requestAccounts" });
+let web3;
+if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
+  //we are in the browser and metamask is running;
+  window.ethereum.request({ method: "eth_requestAccounts" });
+  web3 = new Web3(window.ethereum);
+} else {
+  //we are on the server OR user is not running metamask
+  const provider = new Web3.providers.HttpProvider(
+    "https://rinkeby.infura.io/v3/49365133728c45fd9f0d64f45c920125"
+  );
 
-const web3 = new Web3(window.ethereum);
+  web3 = new Web3(provider);
+}
 
 export default web3;
